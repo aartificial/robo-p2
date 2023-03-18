c05a28c1e80103d7ded8e41bc1a623f47635e440
sol <sol.recieve@gmail.com>
2023-03-18 23:52:24 +0100

    draw-cube

--- draw_cube/src/main.cpp
+++ draw_cube/src/main.cpp
@@ -3,230 +3,165 @@
 #include <opencv2/core.hpp>
 #include <opencv2/opencv.hpp>
 #include <vector>
-#include <iostream>
-#include <cstdlib>
-
 
 namespace {
-const char* about = "Draw cube on ArUco marker images";
 const cv::String &keys =
-    "{@name  |      | Dictionary name in OpenCV predefined dictionaries }"
-    "{@length  |      | Marker side length (in meters). Real world size }"
-    "{@id  |      | Marker id }";
+    "{name  |      | Dictionary name in OpenCV predefined dictionaries }"
+    "{length  |      | Marker side length (in meters). Real world size }"
+    "{id  |      | Marker id }";
 }
 
-void drawCubeWireframe(
-    cv::InputOutputArray image, cv::InputArray cameraMatrix,
-    cv::InputArray distCoeffs, cv::InputArray rvec, cv::InputArray tvec,
-    float l
-);
+/**
+ * @brief Draw axis on image
+ * @param imageCopy
+ * @param axis
+ * @param tvecs
+ */
+void draw_info(const cv::Mat &imageCopy, std::ostringstream &axis, const std::vector<cv::Vec3d> &tvecs);
 
+/**
+ * @brief Draw a cube with wireframe
+ * @param image
+ * @param cameraMatrix
+ * @param distCoeffs
+ * @param rvec
+ * @param tvec
+ * @param l
+ */
+void drawCubeWireframe(cv::InputOutputArray image, cv::InputArray cameraMatrix,cv::InputArray distCoeffs, cv::InputArray rvec, cv::InputArray tvec,float l);
 
-int main(int argc, char **argv)
-{
+/**
+ * @brief Dictionary of aruco markers
+ */
+std::map<std::string, int> aruco_dict = {
+        {"DICT_4X4_50", 0},
+        {"DICT_4X4_100", 1},
+        {"DICT_4X4_250", 2},
+        {"DICT_4X4_1000", 3},
+        {"DICT_5X5_50", 4},
+        {"DICT_5X5_100", 5},
+        {"DICT_5X5_250", 6},
+        {"DICT_5X5_1000", 7},
+        {"DICT_6X6_50", 8},
+        {"DICT_6X6_100", 9},
+        {"DICT_6X6_250", 10},
+        {"DICT_6X6_1000", 11},
+        {"DICT_7X7_50", 12},
+        {"DICT_7X7_100", 13},
+        {"DICT_7X7_250", 14},
+        {"DICT_7X7_1000", 15},
+        {"DICT_ARUCO_ORIGINAL", 16},
+        {"DICT_APRILTAG_16h5", 17},
+        {"DICT_APRILTAG_25h9", 18},
+        {"DICT_APRILTAG_36h10", 19},
+        {"DICT_APRILTAG_36h11", 20}
+};
+
+/**
+ * @brief Main function
+ * @param argc
+ * @param argv
+ */
+int main(int argc, char **argv) {
+    // Parse command line arguments
     cv::CommandLineParser parser(argc, argv, keys);
-    parser.about(about);
 
-    if (argc < 2) {
+    // Check for valid input
+    if (argc < 3) {
         parser.printMessage();
-        return 1;
+        return -1;
     }
 
-  
-    
-    float marker_length_m = atof(argv[3]);
-    int id_marker = atoi(argv[2]);
+    // Get command line arguments
+    auto dictionaryName = parser.get<cv::String>("name");
+    auto length = parser.get<float>("length");
+    auto markerId = parser.get<int>("id");
+    auto dictionaryId = aruco_dict[dictionaryName];
     int wait_time = 10;
 
-    if (marker_length_m <= 0) {
-        std::cerr << "marker length must be a positive value in meter" 
-                  << std::endl;
+    if (length <= 0) {
+        std::cerr << "Invalid marker length" << std::endl;
+        return -1;
+    }
+
+    // Open video input
+    cv::VideoCapture input;
+    input.open(0);
+
+    // Check if video input is opened
+    if (!input.isOpened()) {
+        std::cerr << "failed to open video input: " << 0 << std::endl;
         return 1;
     }
 
-	   
-	cv::String nomDict = argv[1];
-
-	int idDiccionari;
-
-
-	if (nomDict == "DICT_4X4_50") {
-	idDiccionari = 0;
-	} else if (nomDict == "DICT_4X4_100") {
-	idDiccionari = 1;
-	} else if (nomDict == "DICT_4X4_250") {
-	idDiccionari = 2;
-	} else if (nomDict == "DICT_4X4_1000") {
-	idDiccionari = 3;
-	} else if (nomDict == "DICT_5X5_50") {
-	idDiccionari = 4;
-	} else if (nomDict == "DICT_5X5_100") {
-	idDiccionari = 5;
-	} else if (nomDict == "DICT_5X5_250") {
-	idDiccionari = 6;
-	} else if (nomDict == "DICT_5X5_1000") {
-	idDiccionari = 7;
-	} else if (nomDict == "DICT_6X6_50") {
-	idDiccionari = 8;
-	} else if (nomDict == "DICT_6X6_100") {
-	idDiccionari = 9;
-	} else if (nomDict == "DICT_6X6_250") {
-	idDiccionari = 10;
-	} else if (nomDict == "DICT_6X6_1000") {
-	idDiccionari = 11;
-	} else if (nomDict == "DICT_7X7_50") {
-	idDiccionari = 12;
-	} else if (nomDict == "DICT_7X7_100") {
-	idDiccionari = 13;
-	} else if (nomDict == "DICT_7X7_250") {
-	idDiccionari = 14;
-	} else if (nomDict == "DICT_8X8_1000") {
-	idDiccionari = 15;
-	} else {
-	idDiccionari = 16;
-	}
-
-    cv::String videoInput = "0";
-    cv::VideoCapture in_video;
-    
-        in_video.open(0);
-    
-
-    if (!parser.check()) {
-        parser.printErrors();
-        return 1;
-    }
-
-    if (!in_video.isOpened()) {
-        std::cerr << "failed to open video input: " << videoInput << std::endl;
-        return 1;
-    }
-
-    cv::Mat image, image_copy;
+    cv::Mat image, imageCopy;
     cv::Mat camera_matrix, dist_coeffs;
-    std::ostringstream vector_to_marker;
+    std::ostringstream axis;
 
-    cv::Ptr<cv::aruco::Dictionary> dictionary =    cv::aruco::getPredefinedDictionary( \
-        cv::aruco::PREDEFINED_DICTIONARY_NAME(idDiccionari));
+    cv::Ptr<cv::aruco::Dictionary> dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::PREDEFINED_DICTIONARY_NAME(dictionaryId));
 
+    // Read calibration file
     cv::FileStorage fs("fitxerCalibracio.yaml", cv::FileStorage::READ);
 
     fs["camera_matrix"] >> camera_matrix;
     fs["distortion_coefficients"] >> dist_coeffs;
 
-    std::cout << "camera_matrix\n"
-              << camera_matrix << std::endl;
-    std::cout << "\ndist coeffs\n"
-              << dist_coeffs << std::endl;
+    std::cout << "camera_matrix\n" << camera_matrix << std::endl;
+    std::cout << "\ndist coeffs\n" << dist_coeffs << std::endl;
 
-    int frame_width = in_video.get(cv::CAP_PROP_FRAME_WIDTH);
-    int frame_height = in_video.get(cv::CAP_PROP_FRAME_HEIGHT);
-    int fps = 30;
-    int fourcc = cv::VideoWriter::fourcc('M', 'J', 'P', 'G');
-    cv::VideoWriter video(
-        "out.avi", fourcc, fps, cv::Size(frame_width, frame_height), true
-    );
+    while (input.grab()) {
+        input.retrieve(image);
+        image.copyTo(imageCopy);
 
-    while (in_video.grab())
-    {
-
-        in_video.retrieve(image);
-        image.copyTo(image_copy);
+        // detect markers
         std::vector<int> ids;
         std::vector<std::vector<cv::Point2f>> corners;
         cv::aruco::detectMarkers(image, dictionary, corners, ids);
 
         // if at least one marker detected
-         if (ids.size() > 0)
-        {
-            cv::aruco::drawDetectedMarkers(image_copy, corners, ids);
+        if (ids.size() > 0) {
+            cv::aruco::drawDetectedMarkers(imageCopy, corners, ids);
             std::vector<cv::Vec3d> rvecs, tvecs;
-            cv::aruco::estimatePoseSingleMarkers(
-                corners, marker_length_m, camera_matrix, dist_coeffs,
-                rvecs, tvecs
-            );
+            cv::aruco::estimatePoseSingleMarkers(corners, length, camera_matrix, dist_coeffs,rvecs, tvecs);
 
             // draw axis for each marker
-            
-            
-            	std::vector < int > ::iterator itId;
-			itId = find(ids.begin(), ids.end(), atoi(argv[2]));
-			if (itId != ids.end()) {
-			int i = distance(ids.begin(), itId);
-
-			drawCubeWireframe(
-                   	 image_copy, camera_matrix, dist_coeffs, rvecs[i], tvecs[i],
-                   	 marker_length_m
-                	);
-		}
-                
-                
-
-
-                vector_to_marker.str(std::string());
-                vector_to_marker << std::setprecision(4)
-                                 << "x: " << std::setw(8) << tvecs[0](0);
-                cv::putText(image_copy, vector_to_marker.str(),
-                            cv::Point(10, 30), cv::FONT_HERSHEY_SIMPLEX, 0.6,
-                            cv::Scalar(0, 252, 124), 1, CV_AVX);
-
-                vector_to_marker.str(std::string());
-                vector_to_marker << std::setprecision(4)
-                                 << "y: " << std::setw(8) << tvecs[0](1);
-                cv::putText(image_copy, vector_to_marker.str(),
-                            cv::Point(10, 50), cv::FONT_HERSHEY_SIMPLEX, 0.6,
-                            cv::Scalar(0, 252, 124), 1, CV_AVX);
-
-                vector_to_marker.str(std::string());
-                vector_to_marker << std::setprecision(4)
-                                 << "z: " << std::setw(8) << tvecs[0](2);
-                cv::putText(image_copy, vector_to_marker.str(),
-                            cv::Point(10, 70), cv::FONT_HERSHEY_SIMPLEX, 0.6,
-                            cv::Scalar(0, 252, 124), 1, CV_AVX);
-            
+            std::vector < int > ::iterator itId;
+            auto index = std::find(ids.begin(), ids.end(), markerId);
+            if (index != ids.end()) {
+                long i = distance(ids.begin(), itId);
+                drawCubeWireframe(imageCopy, camera_matrix, dist_coeffs, rvecs[i], tvecs[i], length);
+                draw_info(imageCopy, axis, tvecs);
+            }
         }
 
-        video.write(image_copy);
-        cv::imshow("Pose estimation", image_copy);
+        cv::imshow("out", imageCopy);
         char key = (char)cv::waitKey(wait_time);
         if (key == 27)
             break;
     }
 
-    in_video.release();
-
+    input.release();
     return 0;
 }
 
-void drawCubeWireframe(
-    cv::InputOutputArray image, cv::InputArray cameraMatrix,
-    cv::InputArray distCoeffs, cv::InputArray rvec, cv::InputArray tvec,
-    float l
-)
-{
-
-    CV_Assert(
-        image.getMat().total() != 0 &&
-        (image.getMat().channels() == 1 || image.getMat().channels() == 3)
-    );
-    CV_Assert(l > 0);
-    float half_l = l / 2.0;
+void drawCubeWireframe(cv::InputOutputArray image, cv::InputArray cameraMatrix,cv::InputArray distCoeffs, cv::InputArray rvec, cv::InputArray tvec,float l) {
+    // cube half-length
+    auto halfLength = (float) (l / 2.0);
 
     // project cube points
     std::vector<cv::Point3f> axisPoints;
-    axisPoints.push_back(cv::Point3f(half_l, half_l, l));
-    axisPoints.push_back(cv::Point3f(half_l, -half_l, l));
-    axisPoints.push_back(cv::Point3f(-half_l, -half_l, l));
-    axisPoints.push_back(cv::Point3f(-half_l, half_l, l));
-    axisPoints.push_back(cv::Point3f(half_l, half_l, 0));
-    axisPoints.push_back(cv::Point3f(half_l, -half_l, 0));
-    axisPoints.push_back(cv::Point3f(-half_l, -half_l, 0));
-    axisPoints.push_back(cv::Point3f(-half_l, half_l, 0));
+    axisPoints.emplace_back(halfLength, halfLength, l);
+    axisPoints.emplace_back(halfLength, -halfLength, l);
+    axisPoints.emplace_back(-halfLength, -halfLength, l);
+    axisPoints.emplace_back(-halfLength, halfLength, l);
+    axisPoints.emplace_back(halfLength, halfLength, 0);
+    axisPoints.emplace_back(halfLength, -halfLength, 0);
+    axisPoints.emplace_back(-halfLength, -halfLength, 0);
+    axisPoints.emplace_back(-halfLength, halfLength, 0);
 
+    // project axis points
     std::vector<cv::Point2f> imagePoints;
-    projectPoints(
-        axisPoints, rvec, tvec, cameraMatrix, distCoeffs, imagePoints
-    );
+    projectPoints(axisPoints, rvec, tvec, cameraMatrix, distCoeffs, imagePoints);
 
     // draw cube edges lines
     cv::line(image, imagePoints[0], imagePoints[1], cv::Scalar(255, 0, 0), 3);
@@ -241,4 +176,19 @@ void drawCubeWireframe(
     cv::line(image, imagePoints[4], imagePoints[7], cv::Scalar(255, 0, 0), 3);
     cv::line(image, imagePoints[5], imagePoints[6], cv::Scalar(255, 0, 0), 3);
     cv::line(image, imagePoints[6], imagePoints[7], cv::Scalar(255, 0, 0), 3);
+}
+
+
+void draw_info(const cv::Mat &imageCopy, std::ostringstream &axis, const std::vector<cv::Vec3d> &tvecs) {
+    axis.str(std::string());
+    axis << std::setprecision(4) << "x: " << std::setw(8) << tvecs[0](0);
+    cv::putText(imageCopy, axis.str(),cv::Point(10, 30), cv::FONT_HERSHEY_SIMPLEX, 0.6,cv::Scalar(0, 252, 124), 1, CV_AVX);
+
+    axis.str(std::string());
+    axis << std::setprecision(4) << "y: " << std::setw(8) << tvecs[0](1);
+    cv::putText(imageCopy, axis.str(),cv::Point(10, 50), cv::FONT_HERSHEY_SIMPLEX, 0.6,cv::Scalar(0, 252, 124), 1, CV_AVX);
+
+    axis.str(std::string());
+    axis << std::setprecision(4) << "z: " << std::setw(8) << tvecs[0](2);
+    cv::putText(imageCopy, axis.str(),cv::Point(10, 70), cv::FONT_HERSHEY_SIMPLEX, 0.6,cv::Scalar(0, 252, 124), 1, CV_AVX);
 }
